c91a7c60ae111aa3ab924cf7fe4914731370d366
Yana Guseva <jangus@bk.ru>
2018-12-02 17:34:08 +0400

    добавлены schema.sql и data.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -123,3 +123,4 @@ CREATE INDEX IX_Position_name ON Position (name);
 
 CREATE INDEX IX_Organization_Name ON Organization (name);
 CREATE INDEX IX_Organization_Is_Active ON Organization (is_active);
+
